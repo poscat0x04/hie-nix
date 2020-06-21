fe3d22a481ca3a99607a11337577c6c399a25fee
Tony O <822863+bqv@users.noreply.github.com>
2020-06-21 14:57:44 +0100

    Fix source more

--- source/default.nix
+++ source/default.nix
@@ -3,8 +3,8 @@ let
   inherit (bootstrap) fetchFromGitHub;
   loadJSON = f: fetchFromGitHub (builtins.fromJSON (builtins.readFile f));
   sources = rec {
-    "hie-1.3" = ./spec/hie-1.3.json
-    "hie-1.4" = ./spec/hie-1.4.json; 
+    "hie-1.3" = ./spec/hie-1.3.json;
+    "hie-1.4" = ./spec/hie-1.4.json;
     hie-master = ./spec/hie-master.json;
     haskell-nix = ./spec/haskell-nix.json;
   };
